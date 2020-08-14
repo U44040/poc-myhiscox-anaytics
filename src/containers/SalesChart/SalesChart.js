802bf650b01c36210860c24a9fa400983d3ca639
Ruben Arroyo <ruben.rodriguez@hiscox.com>
2020-08-14 12:36:28 +0200

    Functional filters

--- src/containers/SalesChart/SalesChart.js
+++ src/containers/SalesChart/SalesChart.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import BubbleChart from '../../components/Chart/Bubblechart/BubbleChart';
 import Card from '../../components/Shared/Card/Card';
-
 import * as DataGenerator from '../../utils/DataGenerator';
+import rfdc from 'rfdc';
+import * as FILTER_TYPES from '../../components/SidebarFilters/FilterTypes';
 
+const deepClone = rfdc();
 const INTERVAL_REFRESH = 1000;
 
 class SalesChart extends Component {
@@ -12,9 +14,11 @@ class SalesChart extends Component {
     super();
     const data = this.getData();
     const validData = this.getValidData(data);
+    const filteredData = validData;
     this.state = {
       data,
       validData,
+      filteredData,
       averageSales: DataGenerator.averageSales,
     }
   }
@@ -22,6 +26,19 @@ class SalesChart extends Component {
   componentDidMount = () => {
     this.props.updateData(this.state.validData);
     this.setIntervalRefresh(INTERVAL_REFRESH);
+    this.setState((oldState, oldProps) => ({
+      filteredData: this.filterData(oldState.validData)
+    }))
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.filters != this.props.filters) {
+      this.setState((oldState, oldProps) => (
+        {
+          filteredData: this.filterData(oldState.validData)
+        }
+      ));
+    }
   }
 
   getData = () => {
@@ -39,13 +56,47 @@ class SalesChart extends Component {
     })
   }
 
+  concatTypeValue = (type, value) => (type + "_" + value);
+
+  filterData = (data) => {
+    let filteredData = deepClone(data);
+    for (let filterType in this.props.filters){
+      let filters = this.props.filters[filterType].map(d=>d.value);
+      switch (filterType) {
+        case FILTER_TYPES.STATE:
+          for (let state of filteredData) {
+            if (filters.includes(this.concatTypeValue(FILTER_TYPES.STATE, state.state)) == false) {
+              state.projects = [];
+            }
+          }
+          break;
+
+        case FILTER_TYPES.BROKER:
+          for (let state of filteredData) {
+            state.projects = state.projects.filter(d => filters.includes(this.concatTypeValue(FILTER_TYPES.BROKER, d.user.brokerage.id)));
+          }
+          break;
+
+        case FILTER_TYPES.NETWORK:
+          for (let state of filteredData) {
+            state.projects = state.projects.filter(d => filters.includes(this.concatTypeValue(FILTER_TYPES.NETWORK, d.user.brokerage.network.id)));
+          }
+          break;
+      }
+    }
+
+    return filteredData;
+  }
+
   updateData = () => {
     // Update data
     let updatedData = DataGenerator.updateData(this.state.data);
     let validData = this.getValidData(updatedData);
+    let filteredData = this.filterData(validData);
     this.setState({
       data: updatedData,
       validData: validData,
+      filteredData: filteredData,
     }, () => this.props.updateData(validData));
   }
 
@@ -56,7 +107,7 @@ class SalesChart extends Component {
   render = () => (
     <div className="col-md">
       <Card type="primary" header="Venta de pólizas" title="Tiempo real" text="Gráfica en tiempo real de las pólizas que se están creando">
-        <BubbleChart data={this.state.validData} />
+        <BubbleChart data={this.state.filteredData} />
       </Card>
     </div>
   );
