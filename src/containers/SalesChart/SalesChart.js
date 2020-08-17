ee30c60c620ff140846674995d7acee0854aa8f1
Ruben Arroyo <ruben.rodriguez@hiscox.com>
2020-08-17 10:05:44 +0200

    simulate one day

--- src/containers/SalesChart/SalesChart.js
+++ src/containers/SalesChart/SalesChart.js
@@ -4,6 +4,7 @@ import Card from '../../components/Shared/Card/Card';
 import * as DataGenerator from '../../utils/DataGenerator';
 import rfdc from 'rfdc';
 import * as FILTER_TYPES from '../../components/SidebarFilters/FilterTypes';
+import moment from 'moment';
 
 const deepClone = rfdc();
 const INTERVAL_REFRESH = 1000;
@@ -12,14 +13,17 @@ class SalesChart extends Component {
 
   constructor(props) {
     super();
-    const data = this.getData();
+    const actualMoment = moment().hour(8).minute(0).second(0);
+    const data = this.getData(actualMoment);
     const validData = this.getValidData(data);
     const filteredData = validData;
     this.state = {
+      speed: 100,
       data,
       validData,
       filteredData,
       averageSales: DataGenerator.averageSales,
+      actualMoment,
     }
   }
 
@@ -41,8 +45,8 @@ class SalesChart extends Component {
     }
   }
 
-  getData = () => {
-    const data = DataGenerator.generateData();
+  getData = (actualMoment) => {
+    const data = DataGenerator.generateData(actualMoment);
     return data;
   }
 
@@ -90,13 +94,15 @@ class SalesChart extends Component {
 
   updateData = () => {
     // Update data
-    let updatedData = DataGenerator.updateData(this.state.data);
+    let actualMoment = this.state.actualMoment.clone().add(30 * this.state.speed / 100, 'seconds');
+    let updatedData = DataGenerator.updateData(this.state.data, actualMoment);
     let validData = this.getValidData(updatedData);
     let filteredData = this.filterData(validData);
     this.setState({
       data: updatedData,
       validData: validData,
       filteredData: filteredData,
+      actualMoment: actualMoment,
     }, () => this.props.updateData(validData));
   }
 
@@ -104,9 +110,21 @@ class SalesChart extends Component {
     window.setInterval(this.updateData, interval);
   }
 
+  updateSpeed = (e) => {
+    console.log(e.target.value);
+    this.setState(
+      {
+        speed: e.target.value,
+      }
+    )
+  }
+
   render = () => (
     <div className="col-md">
       <Card type="primary" header="Venta de pólizas" title="Tiempo real" text="Gráfica en tiempo real de las pólizas que se están creando">
+        <div>
+          Hora: {this.state.actualMoment.format('HH:mm:ss')} - Velocidad: <input type="range" min="0" max="200" value={this.state.speed} onChange={this.updateSpeed} /> {this.state.speed}%
+        </div>
         <BubbleChart data={this.state.filteredData} />
       </Card>
     </div>
